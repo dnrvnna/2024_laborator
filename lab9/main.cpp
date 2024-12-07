a15210a5204d508570b47c37d4479d1a7c9508b1
dnrvnna <albina.sabirova@internet.ru>
2024-12-07 19:42:14 +0200

    add: lab 9

--- lab9/main.cpp
+++ lab9/main.cpp
@@ -1,55 +1,21 @@
 #include <iostream>
-#include <iomanip>
-
 
 using namespace std;
 
-int sum_of_digit(int x)
-{
-    int s = 0;
-    while (x > 0)
-    {
-        s += x % 10;
-        x = x / 10;
-    }
-    return s;
-}
-    
-    
-int main()
-{ 
-    int n;
-    cin >> n;
-    int * a = new int [n];
-    
-    for ( int i = 0; i < n; i++) cin >> a[i];
-    
-    bool number_exists = false;
-    
-    for (int i = 0; i < n; i++)
-    {
-        if(sum_of_digit(a[i]) == 14)
-        {
-            number_exists = true;
-            break;
-        }
-    }
-    
-    if (number_exists)
-    {
-        for( int i = 0; i < n; i++)
-            for( int j = i + 1; j < n; j++)
-            {
-                if(a[i] > a[j]) swap(a[i], a[j]);
-            }
-    }
-    
+//вариант 3
 
-	for (int i = 0; i < n; i++)
-	{
-		cout << a[i] << " ";
-	}
-	
-    delete [] a;
-	return 0;
+int main()
+{
+    short a;
+    cin >> a;
+    
+    unsigned char * p = (unsigned char * ) &a;
+    
+    for ( int i = 0; i < sizeof(a); i++)
+    {
+        int b = *(p + i);
+        cout << hex << b << " ";
+    }
+    
+    return 0;
 }
